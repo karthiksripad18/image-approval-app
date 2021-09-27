aae0fa588b4ca552695cc4b77a49f3d6ef7cd394
karthiksripad18 <karthiksripad18@gmail.com>
2021-09-28 00:13:33 +0530

    added a new test case

--- src/components/ApprovedImages/ApprovedImages.test.js
+++ src/components/ApprovedImages/ApprovedImages.test.js
@@ -20,33 +20,31 @@ const setUp = (initialState) => {
 }
 
 describe ("Approved Images Test Suite", () => {
-    it ('Should render default icon', () => {
+    it ('Should render heading', () => {
         const state = [];
     
         setUp (state);
         const headingElem = screen.getByText (/approved images/i);
         expect (headingElem).toBeInTheDocument();
     });
+
+    it ('Should render heading with correct number', () => {
+        const state = [
+            {id: "abcd1234", url : {smallSize: "", fullSize: ""}, description: ""},
+            {id: "abcd1234", url : {smallSize: "", fullSize: ""}, description: ""},
+        ];
+    
+        setUp (state);
+        const headingElem = screen.getByText ("approved images (2)");
+        expect (headingElem).toBeInTheDocument();
+    });
     
     it ('Should render Approved Images', () => {
         const state = [
-            {id: "abcd1234", url : {smallSize: "https://images.unsplash.com/photo-1630350684325-2d245f34e2bd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjIxNDl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI3NDk2NTU&ixlib=rb-1.2.1&q=80&w=200", fullSize: "https://images.unsplash.com/photo-1630350684325-2d245f34e2bd?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjIxNDl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI3NDk2NTU&ixlib=rb-1.2.1&q=85"}, description: "test-item"},
+            {id: "abcd1234", url : {smallSize: "", fullSize: ""}, description: ""},
         ]
     
         setUp (state);
-        // console.log("###############", screen.getByRole("list"));
-        expect (screen.getByRole("list")).toBeInTheDocument();
-    
+        expect (screen.getByRole ('list')).toBeInTheDocument();
     });
-
-    // it ('Should render Default Image Container', () => {
-    //     const state = [
-    //         {id: '-w9LXr97FA0', url: {fullSize: "https://images.unsplash.com/photo-1630350684325-2d245f34e2bd?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNjIxNDl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI3NDk2NTU&ixlib=rb-1.2.1&q=85", smallSize: "https://images.unsplash.com/photo-1630350684325-2d245f34e2bd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNjIxNDl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MzI3NDk2NTU&ixlib=rb-1.2.1&q=80&w=200"}, description: 'grayscale photo of girl climbing on spiral staircase', likes: 35, downloads: 5212}
-    //     ];
-    
-    //     setUp (state);
-    //     console.log("###############", screen.getByRole("listitem"));
-    //     // expect (screen.getAllByRole("img").length).toEqual(2);
-    
-    // });
 });
